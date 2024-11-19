4730fe60396fbb69b735b9696e9050f017c5a81b
Matthew Studzinski <88520969+MattStudzinski@users.noreply.github.com>
2024-11-18 22:34:43 -0800

    trying to map over items

--- src/components/Warehouse/Warehouse.jsx
+++ src/components/Warehouse/Warehouse.jsx
@@ -5,6 +5,7 @@ const Warehouse = () => {
 
     const { id } = useParams()
     const [data, setData] = useState(null)
+    const [groupedData, setGroupedData] = useState({})
     const [activeSection, setActiveSection] = useState("")
 
 
@@ -14,6 +15,19 @@ const Warehouse = () => {
             try {
                 const response = await fetch(`http://localhost:5002/warehouses/${id}`)
                 const result = await response.json()
+                console.log("fetched data:", result)
+                console.log("products data", result.products)
+
+                if(result.products) {
+                    const grouped = result.product.reduce((acc, product) => {
+                        const trend = products.trend.toLocaleLowerCase().replace(" ", "_")
+                        if (!acc[trend]) acc[trend] = []
+                        acc[trend].push(product)
+                        return acc
+                    }, {})
+
+                    setGroupedData(grouped)
+                }
                 setData(result)
             } catch (error) {
                 console.error("Error fetching warehouse data:", error)
@@ -26,14 +40,19 @@ const Warehouse = () => {
         setActiveSection(activeSection === section ? "" : section)
     }
 
+    console.log(data)
+
     if (!data) {
         return <p>Loading warehouse data...</p>
     }
     return (
         <div className='results'>
-            <h1 className='results__title'> Warehouse {data.name}</h1>
+            <h1 className='results__title'> Warehouse {data.fileName}</h1>
             <div className='results__sections'>
-                {["Redistributed", "Maxes Raised", "Maxes Lowered", "Flagged for Removal"].map((section) => (
+                {["Redistributed", "Maxes Raised", "Maxes Lowered", "Flagged for Removal"].map((section) => {
+                    const sectionKey = section.toLowerCase().replace(" ", "_")
+                    const items = groupedData[sectionKey] || []
+                    return (
                     <div key={section} className='results__section'>
                         <div
                             className='results__header'
@@ -43,15 +62,16 @@ const Warehouse = () => {
                         </div>
                         {activeSection === section && (
                             <ul className='results__list'>
-                                {data[section.toLocaleLowerCase().replace(" ", "_")].map((item) => {
+                                {items.map((item) => (
                                     <li key={item.itemNumber} className='results__item'>
-                                        <stong>{item.itemNumber}</stong>: {item.Description} - Current Max: {item.maxThreshold}
+                                        <stong>{item.itemNumber}</stong>: {item.description} - Current Max: {item.maxThreshold}
                                     </li>
-                                })}
+                                ))}
                             </ul>
                         )}
                     </div>
-                ))}
+                    )
+                })}
             </div>
         </div>
     );
